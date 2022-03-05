56a833b79e9b62184a49b4dc616a17c6ee9969d4
electrodrago <quocviet11102001@gmail.com>
2022-03-05 09:18:21 +0700

    Login server

--- login.js
+++ login.js
@@ -9,3 +9,20 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+// Listen for create account form submit
+document.getElementById("create_account_fire").addEventListener('submit', submit_create);
+
+document.getElementById("sign_in_fire").addEventListener('submit', submit_sign_in);
+
+function submit_create(e) {
+	e.preventDefault();
+
+	console.log(123);
+}
+
+function submit_sign_in(e) {
+	e.preventDefault();
+
+	console.log(456);
+}
