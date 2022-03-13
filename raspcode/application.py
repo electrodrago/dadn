4a1891f6374b48372a182fd41c2e96bddd2feb79
electrodrago <quocviet11102001@gmail.com>
2022-03-13 20:08:57 +0700

    Threshold - blur detection

--- raspcode/application.py
+++ raspcode/application.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import time
 from cv2 import cv2
 from PIL import Image, ImageTk
+from threading import Thread
 
 # Variable for test application
 concat = ['Tien - Teach1', 'Viet - Teach2']
@@ -11,6 +12,7 @@ course_lst = ['IELTS', 'cal1']
 # Global variable that change when click button of tkinter
 user_to_access_firebase = ""
 course_to_access_firebase = ""
+path_to_img = ""
 
 # Get id on firestore
 def query_id(name_id ,sf):
@@ -21,15 +23,15 @@ class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.container = tk.Frame(self)
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (chooseTeacherPage, chooseCoursePage, chooseMarkingPage, chooseCameraPage):
+        for F in (chooseTeacherPage, chooseCoursePage, chooseMarkingPage, chooseCameraPage, chooseCapturePage):
             page_name = F.__name__
-            frame = F(parent=container, controller=self)
+            frame = F(parent=self.container, controller=self)
             self.frames[page_name] = frame
 
             # put all of the pages in the same location;
@@ -278,7 +280,11 @@ class chooseCameraPage(tk.Frame):
         video_stream()
 
         def assign_and_next_frame():
-            controller.show_frame('chooseTeacherPage')
+            _, img = cap.read()
+            cv2.imwrite('capture/capture.png', img)
+            path_to_img = "capture/capture.png"
+            
+            controller.show_frame('chooseCapturePage')
                 
         marking_button = tk.Button(self,
             text="Capture",
@@ -310,6 +316,98 @@ class chooseCameraPage(tk.Frame):
         time_label.pack(side='right')
 
         tick()
+
+
+class chooseCapturePage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent,bg='#00CC99')
+        self.controller = controller
+
+        self.controller.title('Capture view')
+        # self.controller.state('zoomed')
+        #self.controller.iconphoto(False,tk.PhotoImage(file='C:/Users/urban boutique/Documents/atm tutorial/atm.png'))
+
+        # image = Image.open("capture/capture.png")
+        # test = ImageTk.PhotoImage(image)
+
+        # image_cv2 = cv2.imread("capture/capture.png")
+        # gray = cv2.cvtColor(image_cv2, cv2.COLOR_BGR2GRAY)
+        # blurry = cv2.Laplacian(gray, cv2.CV_64F).var()
+
+        label = tk.Label(self, width=145, height=200)
+        label.pack(pady=2)
+        self.blurry = 100
+        self.text = tk.StringVar()
+        
+        def image_change():
+            # image_cv2 = cv2.imread("capture/capture.png")
+            image_cv2 = cv2.imread("capture/capture.png")
+            gray = cv2.cvtColor(image_cv2, cv2.COLOR_BGR2GRAY)
+            self.blurry = cv2.Laplacian(gray, cv2.CV_64F).var()
+            if self.blurry >= 100:
+                text = "{} - Not blur - Continue".format(int(self.blurry))
+            else:
+                text = "{} - Blur - Recapture".format(int(self.blurry))
+            print(self.blurry)
+            
+            self.text.set(text)
+            image = Image.open("capture/capture.png")
+            test = ImageTk.PhotoImage(image)
+            label.image = test
+            label.configure(image=test)
+            label.after(1000, image_change)
+            
+        
+        image_change()
+
+        
+
+        def get_display_text(blurry, threshold=100):
+            if blurry >= threshold:
+                return "{} - Not blur - Continue".format(int(blurry))
+            else:
+                return "{} - Blur - Recapture".format(int(blurry))
+
+        def assign_and_next_frame(blurry, threshold=100):
+            
+            if blurry >= threshold:
+                controller.show_frame('chooseTeacherPage') # Next frame
+            else:
+                controller.show_frame('chooseCameraPage') # Capture frame
+
+                
+        marking_button = tk.Button(self,
+            textvariable=self.text,
+            command=lambda:assign_and_next_frame(self.blurry),
+            relief='raised',
+            borderwidth = 1,
+            width=20,
+            height=1,
+            font=('orbitron',15, 'bold'),
+            fg="#6666CC"
+        )
+        marking_button.pack(pady=1)
+
+        bottom_frame = tk.Frame(self,relief='raised',borderwidth=3)
+        bottom_frame.pack(fill='x',side='bottom')
+
+        # # TODO: Add later
+        # cse_photo = tk.PhotoImage(file='image/cse_r.png')
+        # cse_label = tk.Label(bottom_frame,image=cse_photo)
+        # cse_label.pack(side='left')
+        # cse_label.image = cse_photo
+
+        def tick():
+            current_time = time.strftime('%I:%M %p').lstrip('0').replace(' 0',' ')
+            time_label.config(text=current_time)
+            time_label.after(200,tick)
+            
+        time_label = tk.Label(bottom_frame,font=('orbitron',12))
+        time_label.pack(side='right')
+
+        tick()
+
 
 if __name__ == "__main__":
     app = App()
