2fcd3c44c67ded88a0fd1ae5f0b91d6cb4fd9ff3
Vinh Tien <vinhtien1612@gmail.com>
2022-04-22 19:25:49 +0700

    add install library files

---
+++ raspcode/install_lib/install.sh
@@ -0,0 +1,13 @@
+sudo apt install build-essential cmake pkg-config -y
+sudo apt install libjpeg-dev libtiff5-dev libpng-dev -y
+sudo apt install libavcodec-dev libavformat-dev -y
+sudo apt install libxvidcore-dev libx264-dev -y
+sudo apt install libfontconfig1-dev libcairo2-dev -y
+sudo apt install libgdk-pixbuf2.0-dev libpango1.0-dev -y
+sudo apt install libgtk2.0-dev libgtk-3-dev -y
+sudo apt install libtlas-base-dev gfortran -y
+sudo apt install libhdf5-dev libhdf5-serial-dev libhdf5-103 -y
+sudo apt install python3-pyqt5 -y
+sudo apt install -y  libhdf5-dev libhdf5-serial-dev -y
+
+
---
+++ raspcode/install_lib/install2.sh
@@ -0,0 +1,5 @@
+wget https://drive.google.com/file/d/1tuNTevmFIEEC9eADdFZEQzebi_ewiPoN/view?usp=sharing
+pip3 install numpy==1.19.2
+pip3 install tensorflow-2.6.0-cp39-cp39-linux_aarch64.whl
+pip3 install cvlib
+pip3 install keras==2.6.0
